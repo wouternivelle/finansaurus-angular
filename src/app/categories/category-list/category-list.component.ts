88758096f3439d59dc08f788c91810a5a47afb99
Wouter Nivelle <wouternivelle@gmail.com>
2022-12-15 14:03:01 +0100

    Use transformation helper for ordering categories

--- src/app/categories/category-list/category-list.component.ts
+++ src/app/categories/category-list/category-list.component.ts
@@ -4,6 +4,7 @@ import {MatDialog} from '@angular/material/dialog';
 import {Category} from '../model/category';
 import {CategoryService} from '../../core/services/category/category.service';
 import {NotificationService} from '../../core/services/notification.service';
+import * as TransformationHelper from '../../shared/helper/transformation.helper';
 
 @Component({
   selector: 'app-category-list',
@@ -29,7 +30,7 @@ export class CategoryListComponent implements OnInit {
   private load() {
     this.categoryService.listWithoutSystem()
       .subscribe(categories => {
-        this.categories = this.orderCategories(categories);
+        this.categories = TransformationHelper.orderCategories(categories);
 
         // Collect the parent categories
         this.categories.filter(cat => cat.parent == null).forEach((category: Category) => {
@@ -50,53 +51,6 @@ export class CategoryListComponent implements OnInit {
             this.notificationService.openSnackBar(category.name + ' is deleted');
           });
       }
-    });
-  }
-
-
-  private orderCategories(categories: Category[]): Category[] {
-    if (!categories) {
-      return categories;
-    }
-
-    let result: Category[] = [];
-    const orderedMap: Map<number, Category[]> = new Map<number, Category[]>();
-
-    // Fill the map with the keys
-    categories.filter(cat => !cat.parent).forEach((category: Category) => {
-      orderedMap.set(category.id!, []);
-    });
-
-    // Fill the entries with the categories who have a parent
-    categories.filter(category => category.parent).forEach(category => {
-      if (orderedMap.has(category.parent!)) {
-        orderedMap.get(category.parent!)!.push(category);
-      }
-    });
-
-    // Create the resulting array
-    orderedMap.forEach((value, key) => {
-      const parent = categories.find(cat => cat.id === key)!;
-      result.push(parent);
-
-      // Push kids but sorted
-      result = result.concat(this.sortCategories(value));
-    });
-
-    return result;
-  };
-
-  private sortCategories(categories: Category[]): Category[] {
-    return categories.sort((c1: Category, c2: Category) => {
-      if (c1.name > c2.name) {
-        return 1;
-      }
-
-      if (c1.name < c2.name) {
-        return -1;
-      }
-
-      return 0;
     });
   }
 }
