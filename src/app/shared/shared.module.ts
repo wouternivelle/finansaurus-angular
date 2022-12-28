eb17443d41399ddb9ae0d3ee8e16f64d1ba9b391
Wouter Nivelle <wouternivelle@gmail.com>
2022-12-28 15:41:15 +0100

    Use flex-box instead of flex-layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
-import { CustomMaterialModule } from '../custom-material/custom-material.module';
+import {CustomMaterialModule} from '../custom-material/custom-material.module';
 
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import { ContentPlaceholderAnimationComponent } from './content-placeholder-animation/content-placeholder-animation.component';
-import { LayoutComponent } from './layout/layout.component';
-import { LimitToPipe } from './pipes/limit-to.pipe';
-import { LocalDatePipe } from './pipes/local-date.pipe';
-import { YesNoPipe } from './pipes/yes-no.pipe';
+import {ConfirmDialogComponent} from './confirm-dialog/confirm-dialog.component';
+import {
+  ContentPlaceholderAnimationComponent
+} from './content-placeholder-animation/content-placeholder-animation.component';
+import {LayoutComponent} from './layout/layout.component';
+import {LimitToPipe} from './pipes/limit-to.pipe';
+import {LocalDatePipe} from './pipes/local-date.pipe';
+import {YesNoPipe} from './pipes/yes-no.pipe';
 
 @NgModule({
-  imports: [RouterModule, CustomMaterialModule, ReactiveFormsModule, FlexLayoutModule],
+  imports: [RouterModule, CustomMaterialModule, ReactiveFormsModule],
   declarations: [
     ConfirmDialogComponent,
     ContentPlaceholderAnimationComponent,
@@ -25,7 +26,6 @@ import { YesNoPipe } from './pipes/yes-no.pipe';
   exports: [
     FormsModule,
     ReactiveFormsModule,
-    FlexLayoutModule,
     CustomMaterialModule,
     LimitToPipe,
     ConfirmDialogComponent,
